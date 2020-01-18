5bbe983fdb6f3a30cf6d2809e1b3e74b06069fe5
SauceRelic <39061831+SauceRelic@users.noreply.github.com>
2020-01-18 13:50:03 -0600

    upload Ali's code

--- camera_motor/main/main.c
+++ camera_motor/main/main.c
@@ -1,42 +1,81 @@
+/*
+  Servo Working code
+
+  This code first configures timer 0 to generated 50Hz signal of repeat after 20ms.
+  Then we used LEDC channel 0 to work with timer 0 and PIn 18 (Servo PIN).
+  The code then keeps on generating pulse correspond to duty cycle for angles 0, 45, 90, 135 and 180 degree
+  after delay of 1.5 seconds. Which  positions the servo at these angles.
+  The process keeps on repeating and servo keeps on changing its position after every 1.5 seconds.
+ */
+#include <stdio.h>
 #include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
-#include "esp_system.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
-#include "nvs_flash.h"
-#include "driver/gpio.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/ledc.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    return ESP_OK;
+//Variables to be used by Servo Motor
+// Delay to wait wach Servo Stop (milliseconds)
+#define SERVO_DELAY 1500
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
+#define SERVO_PIN 18
+// Servo Stop Angles
+#define SERVO_ANGLE_COUNT 8
+const int servo_angles[SERVO_ANGLE_COUNT] = { 0, 45, 90, 135, 180, 135, 90, 45 };
+// create servo object to control a servo
+
+static char tag[] = "servo1";
+
+void moveServo_task(void *ignore) {
+	int bitSize = 15;
+	int minValue = 600;  // microseconds (uS)
+	int maxValue = 3500; // microseconds (uS)
+	int duty = (1 << bitSize) * minValue / 20000;
+	int steps = 180;	// Total Degrees
+
+	//Setup 50Hz Wave using Timer 0
+	ledc_timer_config_t timer_conf;
+	timer_conf.duty_resolution = LEDC_TIMER_15_BIT;
+	timer_conf.freq_hz = 50;
+	timer_conf.speed_mode = LEDC_HIGH_SPEED_MODE;
+	timer_conf.timer_num = LEDC_TIMER_0;
+	ledc_timer_config(&timer_conf);
+
+	//Setup LECD Channel 0 to work with Timer 0 on GPIO 16
+	ledc_channel_config_t ledc_conf;
+	ledc_conf.channel = LEDC_CHANNEL_0;
+	ledc_conf.duty = duty;
+	ledc_conf.gpio_num = SERVO_PIN;
+	ledc_conf.intr_type = LEDC_INTR_DISABLE;
+	ledc_conf.speed_mode = LEDC_HIGH_SPEED_MODE;
+	ledc_conf.timer_sel = LEDC_TIMER_0;
+	ledc_channel_config(&ledc_conf);
+	int current_angle_index = 0;
+
+	while (1) {
+		//calculate Change in Duty Cycle with respect to original
+		int degree = servo_angles[current_angle_index];
+		int delta = degree * (maxValue - minValue) / steps;
+		//Update New Duty Cycle to be used at Channel 0
+		ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, duty + delta);
+		ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+		//Increment Angle Index
+		current_angle_index++;
+		//If Angle Index exceed maximum count reset to index 0
+		if (current_angle_index >= SERVO_ANGLE_COUNT)
+			current_angle_index = 0;
+		//Display at Debug Port
+		ESP_LOGD(tag, "Moving Servo at Angle: %d", degree);
+		ESP_LOGD(tag, "Duty Cycle is: %d", duty + delta);
+		//Delay for required Time (SERVO_DELAY)
+		vTaskDelay(SERVO_DELAY / portTICK_PERIOD_MS);
+	} // End loop forever
+
+	vTaskDelete(NULL);
 }
 
-void app_main(void)
-{
-    nvs_flash_init();
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASSWORD,
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
-
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
-    }
+void app_main() {
+	xTaskCreate(&moveServo_task, "sweepServo_task", 2048, NULL, 5, NULL);
+	printf("servo sweep task  started\n");
 }
-
--- gps/main/main.c
+++ gps/main/main.c
@@ -1,42 +1,146 @@
+/* GPS Working Code
+ * Name: Alotaibi, Aali
+ * Date: 11/11/19
+ * Purpose: GPS module for EE 408
+ Latitude / Longitude Data Format
+ $GNRMC,054404.00,A,4302.56296,N,08754.35012,W,0.020,,181219,,,D*78
+
+ This code scans the incoming serial data from GPS device byte by byte. It keeps on
+ looking the $GNRMC or $GPRMC string as shown in above string. after detecting correct start of
+ sentence, code gets the latitude and longitude strings from after 4 the and 6the comma occurring in sentence.
+ Then these strings are converted into float numbers and direction is corrected as +/- depending on E/W and S/N
+ information obtained. Finally the obtained coordinates are displayed on serial terminal.
+
+ */
+
+#include <stdio.h>
 #include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
-#include "esp_system.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
-#include "nvs_flash.h"
+#include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/uart.h"
+#include "sdkconfig.h"
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
+#define GPS_PIN_TXD (17)
+#define GPS_PIN_RXD (16)
+
+#define BUF_SIZE (256)
+
+//GPS Related Variables
+int Gpsdata;             // for incoming serial data
+unsigned int finish =0;  // indicate end of message
+unsigned int pos_cnt=0;  // position counter
+unsigned int lat_cnt=0;  // latitude data counter
+unsigned int log_cnt=0;  // longitude data counter
+unsigned int flg    =0;  // GPS flag
+unsigned int com_cnt=0;  // comma counter
+char lat[20];            // latitude array
+char lg[20];             // longitude array
+float nlat, nlng;
+//Memory allocation for GPS data buffer
+uint8_t data[BUF_SIZE];
+
+//Parse GPS Data
+void Parse_GPS_Data(int data_in)
 {
-    return ESP_OK;
+	if(finish==0){
+		Gpsdata = data_in;
+		flg = 1;
+		if( Gpsdata=='$' && pos_cnt == 0)   // finding GPRMC header
+			pos_cnt=1;
+		if( Gpsdata=='G' && pos_cnt == 1)
+			pos_cnt=2;
+		if( Gpsdata=='P' && pos_cnt == 2)
+			pos_cnt=3;
+		if( Gpsdata=='N' && pos_cnt == 2)
+			pos_cnt=3;
+		if( Gpsdata=='R' && pos_cnt == 3)
+			pos_cnt=4;
+		if( Gpsdata=='M' && pos_cnt == 4)
+			pos_cnt=5;
+		if( Gpsdata=='C' && pos_cnt==5 )
+			pos_cnt=6;
+		if(pos_cnt==6 &&  Gpsdata ==','){   // count commas in message
+			com_cnt++;
+			flg=0;
+		}
+		if(com_cnt==3 && flg==1){
+			lat[lat_cnt++] =  Gpsdata;         // latitude
+			flg=0;
+		}
+		//Check for Negative Latitude
+		if(com_cnt==4 && flg==1){
+			nlat = atof(lat)/100.0;
+			if(Gpsdata == 'S'){
+				nlat *= -1;
+			}
+			lat[lat_cnt++] =  Gpsdata;         // Negative latitude
+			flg=0;
+		}
+		if(com_cnt==5 && flg==1){
+			lg[log_cnt++] =  Gpsdata;         // Longitude
+			flg=0;
+		}
+		//Check for Negative Longitude
+		if(com_cnt==6 && flg==1){
+			nlng = atof(lg) / 100.0;
+			if(Gpsdata == 'W'){
+				nlng *= -1;
+			}
+			lg[lat_cnt++] =  Gpsdata;         // Negative Longitude
+			flg=0;
+		}
+
+		if( Gpsdata == '*' && com_cnt >= 6){
+			com_cnt = 0;                      // end of GPRMC message
+			lat_cnt = 0;
+			log_cnt = 0;
+			flg     = 0;
+			finish  = 1;
+			pos_cnt = 0;
+		}
+	}
 }
 
-void app_main(void)
-{
-    nvs_flash_init();
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASSWORD,
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
-
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
-    }
+//Read GPS data and feed for parsing
+void feed_gps_data(){
+	//uint8_t *data = (uint8_t*) malloc(BUF_SIZE);
+	//uint8_t data[BUF_SIZE];
+	int len = uart_read_bytes(UART_NUM_2, data, BUF_SIZE, 1000 / portTICK_RATE_MS);
+	for(int i=0; i<len; i++){
+		Parse_GPS_Data(data[i]);
+	}
+	//printf("%s",data);
+	//free (data);
 }
 
+//Configure the the serial ports
+void configure_uart(uart_port_t port,  int baud_rate, int PIN_RX, int PIN_TX){
+	uart_config_t uart_config = {.baud_rate = baud_rate,
+			.data_bits = UART_DATA_8_BITS,
+			.parity = UART_PARITY_DISABLE,
+			.stop_bits = UART_STOP_BITS_1,};
+
+	uart_param_config(port, &uart_config);
+	uart_set_pin(UART_NUM_2, PIN_TX, PIN_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+	uart_driver_install(port, BUF_SIZE * 2, BUF_SIZE * 2, 0, NULL, 0);
+}
+
+//Main ESP32 Function
+void app_main() {
+	//Initialize Serial Ports
+	configure_uart(UART_NUM_0,  115200, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+	configure_uart(UART_NUM_2,  9600, GPS_PIN_RXD, GPS_PIN_TXD);
+
+
+
+	//Read Data from GPS and write to USB Serial Port
+	printf("Starting .... \n");
+	while (1) {
+		feed_gps_data();
+		//printf("Latitude  : %s \n", lat);
+		printf("Numeric Latitude  : %f \n", nlat);
+		//printf("Longitude : %s \n", lg);
+		printf("Numeric Longitude : %f \n", nlng);
+		finish = 0;
+	}
+}
