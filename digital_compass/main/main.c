99f9bf998f823abaf72d49e59b1df3a04feb12e1
iansanford1 <58888555+iansanford1@users.noreply.github.com>
2020-02-02 17:50:53 -0600

    fixed i2c data reading

--- digital_compass/main/main.c
+++ digital_compass/main/main.c
@@ -41,7 +41,7 @@ void app_main(void){
 	i2c_master_write_byte(cmd, 0x60, 1); // Config A
 	i2c_master_write_byte(cmd, 0x80, 1); // Enable temp compensation and setup configuration register A according to datasheet guidelines
 	i2c_master_stop(cmd);
-	i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000/portTICK_PERIOD_MS);
+	i2c_master_cmd_begin(I2C_NUM_0, cmd, pdMS_TO_TICKS(1000));
 	i2c_cmd_link_delete(cmd);
 
 	//maybe delete config B if not necessary
@@ -52,7 +52,7 @@ void app_main(void){
 	i2c_master_write_byte(cmd, 0x61, 1); // "Configuration Register B"
 	i2c_master_write_byte(cmd, 0x07, 1); // default setting check for later to know registers
 	i2c_master_stop(cmd);
-	i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000/portTICK_PERIOD_MS);
+	i2c_master_cmd_begin(I2C_NUM_0, cmd, pdMS_TO_TICKS(1000));
 	i2c_cmd_link_delete(cmd);
 
 	//Set value in "Configuration Register C"
@@ -62,7 +62,7 @@ void app_main(void){
 	i2c_master_write_byte(cmd, 0x62, 1); // "Configuration Register C"
 	i2c_master_write_byte(cmd, 0x01, 1); // Setup configuration register C according to datasheet guidelines
 	i2c_master_stop(cmd);
-	i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000/portTICK_PERIOD_MS);
+	i2c_master_cmd_begin(I2C_NUM_0, cmd, pdMS_TO_TICKS(1000));
 	i2c_cmd_link_delete(cmd);
 
 	//Set active register to "Identification Register"
@@ -71,7 +71,7 @@ void app_main(void){
 	ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (I2C_ADDRESS << 1) | I2C_MASTER_WRITE, 1));
 	ESP_ERROR_CHECK(i2c_master_write_byte(cmd, 0x4F, 1)); //"Identification Register"
 	ESP_ERROR_CHECK(i2c_master_stop(cmd));
-	ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_NUM_0, cmd, 100/portTICK_PERIOD_MS));
+	ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_NUM_0, cmd, pdMS_TO_TICKS(100)));
 	i2c_cmd_link_delete(cmd);
 
 	//Get data from Identification Register A, B and C
@@ -82,7 +82,7 @@ void app_main(void){
 	i2c_master_read_byte(cmd, data+1, 0);
 	i2c_master_read_byte(cmd, data+2, 1);
 	ESP_ERROR_CHECK(i2c_master_stop(cmd));
-	ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_NUM_0, cmd, 100/portTICK_PERIOD_MS));
+	ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_NUM_0, cmd, pdMS_TO_TICKS(100)));
 	i2c_cmd_link_delete(cmd);
 
 	while(1) {
@@ -90,32 +90,32 @@ void app_main(void){
 		cmd = i2c_cmd_link_create();
 		i2c_master_start(cmd);
 		i2c_master_write_byte(cmd, (I2C_ADDRESS << 1) | I2C_MASTER_WRITE, 1);
-		i2c_master_write_byte(cmd, 0x69, 1); //0x69 = "Data Output X MSB Register"
-		i2c_master_stop(cmd);
-		i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000/portTICK_PERIOD_MS);
-		i2c_cmd_link_delete(cmd);
+		i2c_master_write_byte(cmd, 0x68 + 0x80, 1); //0x69 = "Data Output X MSB Register" 0x80 = Auto increment
+//		i2c_master_stop(cmd);
+//		i2c_master_cmd_begin(I2C_NUM_0, cmd, pdMS_TO_TICKS(1000));
+//		i2c_cmd_link_delete(cmd);
 
 		//Read values for X, Y and Z
-		cmd = i2c_cmd_link_create();
+//		cmd = i2c_cmd_link_create();
 		i2c_master_start(cmd);
 		i2c_master_write_byte(cmd, (I2C_ADDRESS << 1) | I2C_MASTER_READ, 1);
-		i2c_master_read_byte(cmd, data,   0); //"Data Output X MSB Register"
-		i2c_master_read_byte(cmd, data+1, 0); //"Data Output X LSB Register"
-		i2c_master_read_byte(cmd, data+2, 0); //"Data Output Z MSB Register"
-		i2c_master_read_byte(cmd, data+3, 0); //"Data Output Z LSB Register"
-		i2c_master_read_byte(cmd, data+4, 0); //"Data Output Y MSB Register"
-		i2c_master_read_byte(cmd, data+5, 1); //"Data Output Y LSB Register "
+		i2c_master_read_byte(cmd, data,   0); //"Data Output X LSB Register"
+		i2c_master_read_byte(cmd, data+1, 0); //"Data Output X MSB Register"
+		i2c_master_read_byte(cmd, data+2, 0); //"Data Output Y LSB Register"
+		i2c_master_read_byte(cmd, data+3, 0); //"Data Output Y MSB Register"
+		i2c_master_read_byte(cmd, data+4, 0); //"Data Output Z LSB Register"
+		i2c_master_read_byte(cmd, data+5, 1); //"Data Output Z MSB Register"
 		i2c_master_stop(cmd);
-		i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000/portTICK_PERIOD_MS);
+		i2c_master_cmd_begin(I2C_NUM_0, cmd, pdMS_TO_TICKS(1000));
 		i2c_cmd_link_delete(cmd);
 
-		short x = data[0] << 8 | data[1];
-		short z = data[2] << 8 | data[3];
-		short y = data[4] << 8 | data[5];
+		short x = data[1] << 8 | data[0];
+		short y = data[3] << 8 | data[2];
+		short z = data[5] << 8 | data[4];
 		int angle = atan2((double)y,(double)x) * (180 / 3.14159265) + 180; // angle in degrees
-		ESP_LOGD(tag, "angle: %d, x: %d, y: %d, z: %d", angle, x, y, z);
-		printf("angle: %d, x: %d, y: %d, z: %d", angle, x, y, z);
-		vTaskDelay(1000/portTICK_PERIOD_MS);
+		//ESP_LOGD(tag, "angle: %d, x: %d, y: %d, z: %d", angle, x, y, z);
+		printf("angle: %d, x: %d, y: %d, z: %d \n", angle, x, y, z);
+		vTaskDelay(pdMS_TO_TICKS(1000));
 	}
 
 	vTaskDelete(NULL);
