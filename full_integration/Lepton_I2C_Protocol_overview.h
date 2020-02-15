867ccdc9d15e4a8836731eb87bb2f1781480548c
Colin Wiese <clnwiese@gmail.com>
2020-02-15 14:47:21 -0600

    The Lepton Overview File
    The overview on the Lepton code

---
+++ full_integration/Lepton_I2C_Protocol_overview.h
@@ -0,0 +1,24 @@
+/*
+ * Lepton_I2C_Protocol_overview.h
+ *
+ *  Created on: Feb 15, 2020
+ *      Author: wiesecl
+ */
+
+#ifndef LEPTON_I2C_PROTOCOL_OVERVIEW_H_
+#define LEPTON_I2C_PROTOCOL_OVERVIEW_H_
+
+
+
+
+
+#endif /* LEPTON_I2C_PROTOCOL_OVERVIEW_H_ */
+
+/* Type: LEP_RESULT
+ * deviceAddress_old = 0x2a;
+ * deviceAddress_new = 0x  ;
+ * -> Define ESP32 address
+ * -> Define ESP32 case
+ * -> Define ESP32 Functions and setup
+ * -> Define ESP32 type
+ */
